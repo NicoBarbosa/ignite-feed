70db1f5b8a5f35024ca7630a442c1c0c4101a3ac
Nicolas Penante <nicolas.penante10@hotmail.com>
2022-11-06 00:35:52 -0300

    :tada: First Commit

---
+++ src/components/Comment.tsx
@@ -0,0 +1,57 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import { useState } from 'react'
+import { Avatar } from './Avatar'
+import styles from './Comment.module.css'
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void
+}
+
+export function Comment({content, onDeleteComment}: CommentProps) {
+
+  const [likeCount, setLikeCount] = useState(0)
+
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+    })
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar 
+        hasBorder={false} 
+        src="https://avatars.githubusercontent.com/u/66042022?v=4"
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Nicolas Penante</strong>
+              <time title="11 de outubro às 15:24h" dateTime='2022-10-12 15:24:22'>Publicado há 1h</time>
+            </div>
+
+            <button onClick={handleDeleteComment} title='Deletar comentário'>
+              <Trash size={24}/>
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
